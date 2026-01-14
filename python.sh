1612311083cf1ce548be15e78e24fee22f11cb5e
Francis Bond <bond@ieee.org>
2026-01-14 17:50:08 +0100

    slides for lecture 4

--- python.sh
+++ python.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mkdir -p etc
+# wget https://github.com/mmihaltz/word2vec-GoogleNews-vectors/raw/refs/heads/master/GoogleNews-vectors-negative300.bin.gz
 
 # Create the virtual environment if missing
 if [ ! -d ".venv-run" ]; then
@@ -8,6 +9,10 @@ if [ ! -d ".venv-run" ]; then
   uv venv '.venv-run' --python 3.12
 fi
 uv pip install wn --python .venv-run
+uv pip install gensim --python .venv-run
+uv pip install transformers --python .venv-run
+uv pip install torch --index-url https://download.pytorch.org/whl/cpu
+
 exec .venv-run/bin/python -i -c "
 import wn
 wn.config.data_directory = 'etc/wn_data'
